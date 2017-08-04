5536ca449fdff981a3e3e320b18d7ed66b058cd1
Takanori Ishibashi <takanori.1112@gmail.com>
2017-08-04 23:24:03 +0900

    Get argv

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,73 @@
 use std::io::{self, BufRead, Write};
+use std::env;
 
 fn main() {
-    let stdin = io::stdin();
+    // let stdin = io::stdin();
 
-    loop {
-        print!(">> ");
-        io::stdout().flush().expect("Error flushing stdout");
+    // loop {
+    //     print!(">> ");
+    //     io::stdout().flush().expect("Error flushing stdout");
 
-        let mut line = String::new();
-        stdin.lock().read_line(&mut line).expect("Error reading from stdin");
+    //     let mut line = String::new();
+    //     stdin.lock().read_line(&mut line).expect("Error reading from stdin");
 
-        println!("{}", eval(read(line)));
+    //     println!("{}", eval(read(line)));
+    // }
+
+    let line =  env::args().nth(1).unwrap();
+    for c in line.trim().chars() {
+        // eval(c);
+        // println!("{}", c);
+        // println!("{}", eval(c));
     }
+    // read(line);
 }
 
 
 fn read(str: String) -> String {
+    println!("{:?}", str);
     str
 }
 
-fn eval(line: String) -> String {
+fn eval(c: char) -> i32 {
+    if c.is_numeric() {
 
+    }
+
+    let x = 1;
+    let y = 2;
+
+    match c {
+            // '(' => println!("{}", c),
+            // ')' => println!("{}", c),
+            '+' => { return x + y; },
+            '-' => { return x - y; },
+            '*' => { return x * y; },
+            '/' => { return x / y; },
+            _ => {
+            }
+    }
+    // println!("{:?}", c);
+    0
+}
+
+fn eval2(line: String) -> i32 {
     for c in line.chars() {
         if c.is_numeric() {
-
         }
 
+        let x = 1;
+        let y = 2;
         match c {
             // '(' => println!("{}", c),
             // ')' => println!("{}", c),
-            '+' => { x + y; },
-            '-' => { x - y; },
-            '*' => { x * y; },
-            '/' => { x / y; },
+            '+' => { return x + y; },
+            '-' => { return x - y; },
+            '*' => { return x * y; },
+            '/' => { return x / y; },
             _ => {
             }
         }
     }
-    line
+    0
 }
