ad994cb8e96bebfe5c229e111717e1ac2556aa33
Takanori Ishibashi <takanori.1112@gmail.com>
2017-08-21 20:51:21 +0900

    Add data structure

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,97 @@
 use std::io::{self, BufRead, Write};
 use std::env;
+use std::collections::HashMap;
+use std::collections::HashSet;
 
-fn main() {
-    // let stdin = io::stdin();
+#[derive(Clone, PartialEq, Eq, Hash, Debug)]
+struct LRef(i64);
 
-    // loop {
-    //     print!(">> ");
-    //     io::stdout().flush().expect("Error flushing stdout");
-
-    //     let mut line = String::new();
-    //     stdin.lock().read_line(&mut line).expect("Error reading from stdin");
-
-    //     println!("{}", eval(read(line)));
-    // }
-
-    let line =  env::args().nth(1).unwrap();
-    for c in line.trim().chars() {
-        // eval(c);
-        // println!("{}", c);
-        // println!("{}", eval(c));
-    }
-    // read(line);
+#[derive(Debug)]
+struct Arena {
+    last: i64,
+    data: HashMap<LRef, LObj>,
 }
 
+impl Arena {
+    fn new() -> Arena {
+        Arena {
+            last: 0,
+            data: HashMap::new(),
+        }
+    }
+
+    fn get(&self, key: &LRef) -> LObj {
+        match self.data.get(&key) {
+            Some(val) => val.clone(),
+            _ => LObj::Nil,
+        }
+    }
+
+    fn set(&mut self, key: LRef, val: LObj) {
+        self.data.insert(key, val);
+    }
+
+    fn make(&mut self, obj: LObj) -> LRef {
+        self.last += 1;
+        self.data.insert(LRef(self.last), obj);
+        LRef(self.last)
+    }
+}
+
+#[derive(Clone, PartialEq, Debug)]
+enum LObj {
+    Nil,
+    Sym(String),
+    Num(i64),
+    Subr(SubFn),
+    Expr(LRef, LRef),
+    Cons(LRef, LRef),
+}
+
+#[derive(Clone, PartialEq, Debug)]
+enum SubFn {
+    Car,
+    Cdr,
+    Cons,
+    Eq,
+    Atom,
+    Numberp,
+    Symbolp,
+    Add,
+    Mul,
+    Sub,
+    Div,
+    Mod,
+    T,
+}
+
+#[derive(Debug)]
+struct Evaluator {
+    arena: Arena,
+    genv: LRef,
+}
 
 fn read(str: String) -> String {
-    println!("{:?}", str);
     str
 }
 
-fn eval(c: char) -> i32 {
-    if c.is_numeric() {
-
-    }
-
-    let x = 1;
-    let y = 2;
-
-    match c {
-            // '(' => println!("{}", c),
-            // ')' => println!("{}", c),
-            '+' => { return x + y; },
-            '-' => { return x - y; },
-            '*' => { return x * y; },
-            '/' => { return x / y; },
-            _ => {
-            }
-    }
-    // println!("{:?}", c);
+fn eval(c: String, arena: &mut Arena) -> i32 {
     0
 }
 
-fn eval2(line: String) -> i32 {
-    for c in line.chars() {
-        if c.is_numeric() {
-        }
+fn main() {
+    let stdin = io::stdin();
+    let mut arena = Arena::new();
 
-        let x = 1;
-        let y = 2;
-        match c {
-            // '(' => println!("{}", c),
-            // ')' => println!("{}", c),
-            '+' => { return x + y; },
-            '-' => { return x - y; },
-            '*' => { return x * y; },
-            '/' => { return x / y; },
-            _ => {
-            }
-        }
+    loop {
+        print!(">> ");
+        io::stdout().flush().expect("Error flushing stdout");
+
+        let mut line = String::new();
+        stdin.lock().read_line(&mut line).expect("Error reading from stdin");
+
+        // println!("{}", read(line));
+        eval(line, &mut arena);
     }
-    0
 }
+
